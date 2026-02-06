177f3c8a395272ebca380f70aaf452e435ac78ab
kalama <kalamayeng@gmail.com>
2026-02-06 17:03:45 +0300

    feat: add rate limiting for ticket retrieval using upstash Redis

--- app/api/bookings/print-ticket/route.ts
+++ app/api/bookings/print-ticket/route.ts
@@ -1,9 +1,61 @@
 import { createClient } from "@/lib/supabase/server";
 import { encrypt } from "@/lib/crypto";
 import { NextResponse } from "next/server";
+import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
+
+// Redis initialization(for rate limiting)
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+});
+
+// 5 request for 10 minutes
+const ratelimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(5, "10 m"),
+  analytics: true,
+  ephemeralCache: new Map(),
+});
 
 export async function POST(req: Request) {
   try {
+    // Get the ip
+    const ip = req.headers.get("x-nf-client-connection-ip") || "127.0.0.1";
+    //Check if user is over the limit
+    const { success, limit, reset, remaining } = await ratelimit.limit(ip);
+    console.log(
+      `success: ${success}, limit: ${limit}, reset${reset}, remaining: ${remaining}`,
+    );
+
+    if (!success) {
+      // Calculate delta in milliseconds
+      const now = Date.now();
+      const diff = reset - now;
+
+      // Convert to minutes and seconds
+      const minutes = Math.floor(diff / 1000 / 60);
+      const seconds = Math.floor((diff / 1000) % 60);
+
+      const waitTime =
+        minutes > 0
+          ? `${minutes} minute${minutes > 1 ? "s" : ""}${seconds > 0 ? ` and ${seconds} second${seconds > 1 ? "s" : ""}` : ""}`
+          : `${seconds} second${seconds > 1 ? "s" : ""}`;
+
+      return NextResponse.json(
+        {
+          success: false,
+          error: `Too many attempts. Please wait ${waitTime} before trying again.`,
+        },
+        {
+          status: 429,
+          headers: {
+            "X-RateLimit-Reset": reset.toString(),
+            "X-RateLimit-Remaining": remaining.toString(),
+          },
+        },
+      );
+    }
     const request = await req.json();
     const supabase = await createClient();
     const ticketNo = request.ticketNumber.toLowerCase();
